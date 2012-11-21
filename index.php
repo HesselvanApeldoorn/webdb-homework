e62b5ebdf032a1a0e162ca8c855cf752b073c879
Hessel <hesselva1@gmail.com>
2012-11-21 14:54:48 +0100

    questions and answers are now retrieved from the database. (Exercise 7)

--- index.php
+++ index.php
@@ -4,35 +4,37 @@
     </head>
     <body>
         <?php
-            $questions = array( 
-                array("How does a duck know what direction south is?", 2, "He doesn't", "Magnetic field", "Sonar", "GPS"),
-                array("How does he tell his wife from all the other ducks?", 1, "wedding ring", "He doesn't", "Feathers", "GPS"),
-            );
-            if ($_REQUEST['questionNo']==0) {
+            if ($_POST['questionNo']==0) {
                 $i=0;
             } else {
-                $i=($_REQUEST['questionNo']);
+                $i=($_POST['questionNo']);
             }
-            if ($i>=count($questions)) {
+            $numberQuestions = mysql_result(mysql_query("select count(*) from question"),0);
+            if ($i>=$numberQuestions) {
                 echo "You've reached the end of this quiz. Thank you for your participation.";
             } elseif (!isset($_REQUEST['submit'])) {
-                echo $questions[$i][0]."</br>
+                $question = mysql_query("select q_text from question where q_number=($i+1)");
+                echo mysql_result($question,0)."</br>
                 <form method='post'>";
-                    for($j=2; $j<count($questions[$i]); $j++) {
-                        echo "<input type = 'radio' name = 'ans' value = '".($j-1)."'> ";
-                        echo $questions[$i][$j]."</br>";
+                    $answers = mysql_query("select * from choice where quest_number=($i+1)");
+                    $j=1;
+                    while($answer = mysql_fetch_array($answers)) {
+                        echo "<input type = 'radio' name = 'ans' value = '".($j)."'> ".$answer['c_text']."<br>";
+                        $j++;
                     }
                     echo " <input type = 'submit' name = 'submit' value = 'submit'>
                     <input type = 'hidden' name = 'questionNo' value ='".$i."'>
                 </form>"; 
             } else {
-                if ($_POST['ans']==$questions[$i][1]) {
+                $correct = mysql_query("select correct from choice where c_number=".($_POST['ans'])." AND quest_number=".($i+1));
+                $correctness = mysql_result($correct,0);
+                if ($correctness) {
                     echo "Good job! That's the correct answer";
                 } else {
                     echo "No, your answer is wrong.";
                 }
                 $i++;
-                echo "<form>
+                echo "<form method='post'>
                     <input type = 'submit' name = 'Next' value = 'Next'>
                     <input type = 'hidden' name = 'questionNo' value ='".$i."'>
                 </form>";
