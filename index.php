df771f99e89999841c88fbee9d52900a0fd25c4a
HesselvanApeldoorn <hesselva1@gmail.com>
2012-11-28 15:52:13 +0100

    Added PDO as database abstraction layer. All queries now use PDO

--- index.php
+++ index.php
@@ -3,70 +3,59 @@
             <?php require_once('../../dbConfig.php'); 
                 error_reporting(-1);
                 ini_set("display_errors", 1);
+                session_start();
             ?>
     </head>
     <body>
         <?php
-	$dsn="mysql://$username:$password@$host/$db";
-	$mdb2=mysql_connect($host, $username, $password);
-           mysql_select_db($db);
-        
-// 	if (DB::isError($mdb2))
-// 	{
-// 	    die($mdb2->getMessage().'  ,  '. $mdb2->getDebugInfo());
-// 	} else {
-// 	    echo $mdb2;
-            if(!isset($_COOKIE["cookie"])) {
-                setcookie("cookie[score]", 0);
-                setcookie("cookie[question]", 0);
+        $con = new PDO("mysql:dbname=$db;host=$host", $username, $password);
+            if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 604800)) {
+                session_unset();
+                session_destroy();
             }
-            if ($_POST['questionNo']==0) {
-                $i=0;
-                setcookie("cookie[score]", 0);
-                setcookie("cookie[question]", 0);
-            } else {
-                $i=($_POST['questionNo']);
+            if(!isset($_SESSION["score"]) | !isset($_SESSION["question"])) {
+                $_SESSION["score"] = 0;
+                $_SESSION["question"]= 0;
             }
-            if($_COOKIE["cookie"]["question"]!=$i) {
-                echo "<h1>You are not at the correct question (we aren't cheating now, are we?). Please redo the entire quiz.</h1>";
-                echo "<a href=''>Retry</a>";
-            } else {
-                $numberQuestions = mysql_result(mysql_query("select count(*) from question"),0);
-                //die($mdb2->getMessage().'  ,  '. $mdb2->getDebugInfo());
-                if ($i>=$numberQuestions) {
-                    echo "You've reached the end of this quiz. Thank you for your participation.<br>";
-                    echo "Your score: ".$_COOKIE["cookie"]["score"]." out of ".$numberQuestions; 
-                    setcookie("cookie[score]", 0);
-                    setcookie("cookie[question]", 0);
-                } elseif (!isset($_REQUEST['submit'])) {
-                    $question = mysql_query("select q_text from question where q_number=($i+1)");
-                    echo mysql_result($question,0)."</br>
-                    <form method='post'>";
-                        $answers = mysql_query("select * from choice where quest_number=($i+1)");
-                        while($answer = mysql_fetch_array($answers)) {
-                            echo "<input type = 'radio' name = 'ans' value = '".$answer['c_number']."'> ".$answer['c_text']."<br>";
-                        }
-                        echo " <input type = 'submit' name = 'submit' value = 'submit'>
-                        <input type = 'hidden' name = 'questionNo' value ='".$i."'>
-                    </form>"; 
-                } else {
-                    $correct = mysql_query("select correct from choice where c_number=".($_POST['ans'])." AND quest_number=".($i+1));
-                    $correctness = mysql_result($correct,0);
-                    setcookie("cookie[question]",$_COOKIE["cookie"]["question"]+1);
-                    if ($correctness) {
-                        setcookie("cookie[score]",$_COOKIE["cookie"]["score"]+1);
-                        echo "Good job! That's the correct answer";
-                    } else {
-                        echo "No, your answer is wrong.";
+            $_SESSION['LAST_ACTIVITY'] = time();
+            $query =0;
+            $query = $con->prepare('select count(*) from question');
+            $query->execute();
+            $numberQuestions = $query->fetchColumn();
+            if ($_SESSION["question"]>=$numberQuestions) {
+                echo "You've reached the end of this quiz. Thank you for your participation.<br>";
+                echo "Your score: ".$_SESSION["score"]." out of ".$numberQuestions; 
+                echo "</br><a href=''>Retry</a>";
+                session_destroy();
+            } elseif (!isset($_REQUEST['submit'])) {
+                $sql="select q_text from question where q_number=".($_SESSION['question']+1);
+                $query = $con->query($sql);
+                $question = $query->fetch();
+                echo $question['q_text'];
+                //echo mysql_result($question,0)."</br>
+                echo "<form method='post'>";
+                    $sql = "select * from choice where quest_number=".($_SESSION['question']+1);
+                    $answers = $con->query($sql);
+                    while($answer = $answers->fetch()) {
+                        echo "<input type = 'radio' name = 'ans' value = '".$answer['c_number']."'> ".$answer['c_text']."<br>";
                     }
-                    $i++;
-                    echo "<form method='post'>
-                        <input type = 'submit' name = 'Next' value = 'Next'>
-                        <input type = 'hidden' name = 'questionNo' value ='".$i."'>
-                    </form>";
+                    echo " <input type = 'submit' name = 'submit' value = 'submit'>
+                </form>"; 
+            } else {
+                $sql = "select correct from choice where c_number=".($_POST['ans'])." AND quest_number=".($_SESSION["question"]+1);
+                $correct = $con->query($sql);
+                $correctness = $correct->fetch();
+                $_SESSION ["question"]+= 1;
+                if ($correctness['correct']) {
+                    $_SESSION ["score"]+= 1;
+                    echo "Good job! That's the correct answer";
+                } else {
+                    echo "No, your answer is wrong.";
                 }
+                echo "<form method='post'>
+                    <input type = 'submit' name = 'Next' value = 'Next'>
+                </form>";
             }
-//         }
         ?>
     </body>
 </html>
